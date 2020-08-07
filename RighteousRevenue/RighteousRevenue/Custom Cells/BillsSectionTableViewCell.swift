532fa59f3f6349d04d34b65f9c6fa68dd1e3ccab
nelsonthegr8 <32414327+nelsonthegr8@users.noreply.github.com>
2020-08-07 00:12:00 -0400

    Added Table View to add and edit section and created cards behind them to make it look pretty

---
+++ RighteousRevenue/RighteousRevenue/Custom Cells/BillsSectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BillsSectionTableViewCell.swift
+//  RighteousRevenue
+//
+//  Created by Nelson Brumaire on 8/6/20.
+//  Copyright © 2020 Nelson Brumaire. All rights reserved.
+//
+
+import UIKit
+
+class BillsSectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- RighteousRevenue/RighteousRevenue/View Controllers/AddEditViewController.swift
+++ RighteousRevenue/RighteousRevenue/View Controllers/AddEditViewController.swift
@@ -8,22 +8,20 @@
 
 import UIKit
 
-class AddEditViewController: UIViewController {
+class AddEditViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var titleTxt: UILabel!
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var tableCardView: UIView!
     @IBOutlet weak var billNameTxtbx: UITextField!
     @IBOutlet weak var billAmountTxtbx: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        cardView.layer.shadowColor = UIColor.gray.cgColor
-        cardView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        cardView.layer.shadowOpacity = 1.0
-        cardView.layer.masksToBounds = false
-        cardView.layer.cornerRadius = 10.0
+        
+        setCardViews()
+        setupTableView()
     }
     
     @IBAction func cancelPressed(_ sender: Any) {
@@ -31,6 +29,35 @@ class AddEditViewController: UIViewController {
     }
     
     @IBAction func addBtnPressed(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          30
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "billsCell", for: indexPath) as! BillsSectionTableViewCell
+        
+        return cell
+      }
+    
+    func setCardViews(){
+        let viewArr:[UIView] = [cardView,tableCardView]
+        // Do any additional setup after loading the view.
+        for view in viewArr{
+            view.layer.shadowColor = UIColor.gray.cgColor
+            view.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+            view.layer.shadowOpacity = 1.0
+            view.layer.masksToBounds = false
+            view.layer.cornerRadius = 10.0
+        }
+    }
+    
+    func setupTableView(){
+        let nib = UINib(nibName: "BillsSectionTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "billsCell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 }
